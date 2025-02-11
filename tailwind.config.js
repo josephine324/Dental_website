252811bb3dbaa1fd8093014fc23132bd21788029
josephine324 <j.mutesi@alustudent.com>
2025-02-11 15:24:30 +0200

    body

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,20 @@ module.exports = {
     extend: {
       colors:{
         darkblue: "#003B5C",
-        customFont: "SofiaPro-Regular",
-        Font: "SofiaPro-Bold",
         newYellow: "#FFEC2D",
         headerblue: "#023D5D",
         pagecolor: "#EDF3F6",
         svgcolor: "#E6EEF2",
-      }
+        meetYellow: "#FFF596",
+        greybg: "#F6F4F3",
+        anothercolor: "#003757",
+        footercolor: "#002748",
+        officecolor: "7F9AAA",
+      },
+      fontFamily: {
+        sofiaRegular: ["SofiaPro-Regular", "sans-serif", "Arial","Helvetica"],
+        sofiaBold: ["SofiaPro-Bold", "sans-serif","sans-serif", "Arial","Helvetica"],
+      },
     },
   },
   plugins: [],
